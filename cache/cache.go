79f76b7625a862093b804171d2490cbe43196d9c
initsuj <initsuj@outlook.com>
2016-03-05 15:38:07 -0700

    add account caching

---
+++ cache/cache.go
@@ -0,0 +1,133 @@
+package cache
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/initsuj/gomc/mcauth"
+)
+
+const (
+	filename = "accounts.json"
+	timeout = time.Duration(100) * time.Millisecond
+)
+
+var (
+	accounts = make(map[string]mcauth.Account)
+	state = &sync.RWMutex{}
+
+	watcher *fsnotify.Watcher
+	timer   *time.Timer
+	done = make(chan bool)
+)
+
+func Init() error {
+	var err error
+	if err = load(); err != nil {
+		return err
+	}
+
+	watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	if err = watcher.Add(filename); err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op & fsnotify.Write == fsnotify.Write {
+					changed()
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	return nil
+}
+
+func Store(a auth.Account) error {
+	state.RLock()
+	accounts[a.Login] = a
+	state.RUnlock()
+
+	return save()
+}
+
+func Find(s string) (a auth.Account) {
+	state.RLock()
+	a = accounts[s]
+	state.RUnlock()
+
+	return a
+}
+
+func Close() {
+	<-done
+	watcher.Close()
+}
+
+func load() error {
+	c, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	state.Lock()
+	if err := json.Unmarshal(c, &accounts); err != nil {
+		return err
+	}
+	state.Unlock()
+
+	return nil
+}
+
+func save() error {
+	state.RLock()
+	contents, err := json.MarshalIndent(accounts, "", "    ")
+	if err != nil {
+		return err
+	}
+	state.RUnlock()
+	runtime.Gosched()
+
+	return write(contents)
+
+}
+
+func write(b []byte) error {
+	err := ioutil.WriteFile(filename, b, 7777)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func changed() {
+	if timer == nil {
+		go func() {
+			timer = time.NewTimer(timeout)
+			<-timer.C
+			timer = nil
+			if err := load(); err != nil {
+				log.Println("Could not update cache: ", err)
+			}
+		}()
+	} else {
+		timer.Reset(timeout)
+	}
+}
+
--- gomc-console.go
+++ gomc-console.go
@@ -5,9 +5,27 @@ import (
 
 	"github.com/initsuj/gomc-console/console"
 	"github.com/initsuj/gomc/mcchat"
+	"flag"
+	"github.com/initsuj/gomc-console/cache"
 )
 
 func main() {
+
+	var ini, server, user, pwd string
+
+	flag.StringVar(&ini, "i", "gomc.ini", "location of ini file to use.")
+	flag.StringVar(&server, "s", "", "server to connect to.")
+	flag.StringVar(&user, "u", "", "mojang account username.")
+	flag.StringVar(&pwd, "p", "", "mojang account password.")
+	flag.Parse()
+
+	if err := cache.Init(); err != nil{
+		panic(err)
+	}
+	defer cache.Close()
+
+
+
 	//fmt.Print("test = " + mcchat.Black)
 	console.Print(mcchat.Yellow, "hello ", mcchat.Red, "Red, ", mcchat.DarkRed, " DarkRed")
 	console.Println(mcchat.Yellow, "hello ", mcchat.Red, "Red, ", mcchat.DarkRed, " DarkRed")
